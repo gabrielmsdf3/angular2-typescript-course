6dfd3534a2f83879829246963d2ba2420f45c944
Gabriel Moura <gabrielmsdf3@gmail.com>
2021-05-01 15:38:57 -0300

    animacao carregamentos + tag ng template

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Produto } from '../objetos/Produto';
-import {ProdutoService} from './../service/produto.service'
+import { ProdutoService } from './../service/produto.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  prod: any
-  produtos: Array<Produto> = []
-  constructor(private produtoService: ProdutoService) { }
+  prod: any;
+  produtos: Array<Produto> = [];
+  carregarLoading: Boolean = false;
+  constructor(private produtoService: ProdutoService) {}
 
   ngOnInit(): void {
-    this.produtoService.listar().subscribe(prods =>{
-      this.produtos = prods
-      this.prod = prods
-      console.log(this.produtos);
-      
-    })
+    this.produtoService.listar().subscribe((prods) => {
+      setTimeout(() => {
+        this.carregarLoading = true;
+        this.produtos = prods;
+      }, 3000);
+    });
   }
-
 }
